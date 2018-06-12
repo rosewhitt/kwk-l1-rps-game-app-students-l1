733a3b7a8b1cdab42b28b2583afb48e7e61ef872
rosewhitt <rosewhitt@icloud.com>
2018-06-12 20:55:15 +0000

    automatically backed up by learn

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -3,6 +3,10 @@ class RPSGame
 
   input = gets.strip
 
+  def input
+    if input =! "rock", "paper", "scissors"
+      puts "invalid"
+
   class PlayTypeError < StandardError
     # Don't be alarmed! This is a class set inside a class, but works just fine in Ruby
     # In this case, we're creating a custom error that should be raised if RPSGame.new receives an invalid move as an argument
