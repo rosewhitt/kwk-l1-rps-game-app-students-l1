c27f5e11c9bdd3dc4776d778ae112e6585e790c6
rosewhitt <rosewhitt@icloud.com>
2018-06-12 20:56:49 +0000

    automatically backed up by learn

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -6,6 +6,7 @@ class RPSGame
   def input
     if input =! "rock"; "paper";"scissors"
       puts "invalid"
+  end
 
   class PlayTypeError < StandardError
     # Don't be alarmed! This is a class set inside a class, but works just fine in Ruby
